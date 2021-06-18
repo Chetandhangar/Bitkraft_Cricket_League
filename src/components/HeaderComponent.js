12fb490a9d3be83982fd983c65bf248adec7420f
Chetan Dhangar <chetansdhangar@gmail.com>
2021-06-18 20:37:10 +0530

    style - headercomponent

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,8 +1,27 @@
 import './styles.css';
+import { useTab } from '../contexts/tab-context';
+
 export function Header(){
+
+    const {setTab} = useTab()
+
+    function tabHandler(tab){
+        setTab(tab)
+    }
+
     return(
         <div className="header">
-           <header>This is header Component</header> 
-        </div>
+        <header className="league">
+          <h1 className="league-name">BitKraft Cricket League</h1>
+        <div className="tabs">
+         <button className="btn-tab" onClick={() => tabHandler("countries")}>Countries</button>
+         <button className="btn-tab" onClick={() => tabHandler("teams")}>Teams</button>
+         <button className="btn-tab" onClick={() => tabHandler("players")}> Players </button>
+         <button className="btn-tab" onClick={() => tabHandler("venues")}>Venues</button>
+         <button className="btn-tab" onClick={() => tabHandler("matches")}>Matches</button>
+         <button className="btn-tab" onClick={() => tabHandler("table")}>Score Table</button>
+         </div>
+         </header>
+       </div>  
     );
 }
--- src/components/players/PlayersComponent.js
+++ src/components/players/PlayersComponent.js
@@ -1,7 +1,7 @@
 import { useBitkraft} from '../../contexts/bitkraft-context';
 import { useTab} from '../../contexts/tab-context';
 import { useCurrentDetails} from '../../contexts/current-context'
-
+import '../players/players.css';
 export function Players() {
 
   const { PLAYERS } = useBitkraft();
@@ -15,14 +15,11 @@ export function Players() {
 
   return (
       <div className="App">
-        <div>
-          Players List
-        </div>
-        <div>
+        <div className="card">
           {PLAYERS.map((player) => (
-            <div key={player.id}>
-               <img src={player.image_url} alt={player.firstname}></img>
-              <h1><small>Name : </small>{`${player.firstname} ${" "} ${player.lastname}`}</h1>
+            <div key={player.id} className="card-player">
+               <img className="player-card-image" src={player.image_url} alt={player.firstname}></img>
+              <h1 className="player-card-text">{`${player.firstname} ${" "} ${player.lastname}`}</h1>
               <button onClick={() => handleClick('player_details',player)}>See More</button>
               </div>
           ))}
